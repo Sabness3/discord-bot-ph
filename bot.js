66df21ab7a7aa3315c1d47ddba184885bc0db27e
Sabness3 <47712839+Sabness3@users.noreply.github.com>
2019-05-06 17:39:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,19 +138,8 @@ bot.on("message", async message => {
 	});
 
 
-  bot.on("message", (message) => {
-  
-    if (message.content.includes("https://discord.gg/")) {
-      console.log("deleted " + message.content + " from " + message.author)
-      message.delete(1);
-      message.channel.sendMessage("No links here, " + message.author)
-    }
-    if (message.content.includes("https://discord.gg/discord-developers")) {
-      console.log("deleted " + message.content + " from " + message.author)
-      message.delete(1);
-      message.channel.sendMessage("No links here, " + message.author)
-      }
-    });
+
+
 
 
 // Tritax AI Login:
